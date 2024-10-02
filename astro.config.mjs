9679cdcc5bd7499ca682639c8ba5f79ec0e37188
Brahm Van Houzen <brahmvanh@gmail.com>
2024-10-02 12:47:39 -0400

    removed ssr stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwind from '@astrojs/tailwind';
-
-import node from '@astrojs/node';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: 'server',
-
-  adapter: node({
-    mode: 'standalone'
-  })
 });
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -17,28 +17,34 @@ export default function ProjectCard({ project }: Readonly<ProjectCardProps>) {
   }, [project.imgKey]);
 
   return (
-    <a
-      target="_blank"
-      rel="noreferrer"
-      href={project.projectUrl}
-      id="project-card"
-      data-aos="fade-left"
-      data-aos-duration="1500"
-      className="relative border rounded-2xl mx-2 group"
-    >
-      <img
-        className="rounded-2xl max-h-[700px] h-[400px] max-w-[5000px]"
-        src={screenshotUrl}
-        alt={project.name}
-      />
-      <div
-        className="absolute rounded-2xl inset-0 bg-black/45 opacity-0 transition-opacity 
-                 duration-300 group-hover:opacity-100 flex flex-col 
-                 justify-center items-center text-white"
-      >
-        <h1 className="text-xl font-bold mb-2">{project.name}</h1>
-        <p className="text-sm p-2">{project.textPreview}</p>
-      </div>
-    </a>
+    <>
+      {screenshotUrl ? (
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href={project.projectUrl}
+          id="project-card"
+          data-aos="fade-left"
+          data-aos-duration="1500"
+          className="relative border rounded-2xl mx-2 group"
+        >
+          <img
+            className="rounded-2xl max-h-[700px] h-[400px] max-w-[5000px]"
+            src={screenshotUrl}
+            alt={project.name}
+          />
+          <div
+            className="absolute rounded-2xl inset-0 bg-black/45 opacity-0 transition-opacity 
+      duration-300 group-hover:opacity-100 flex flex-col 
+      justify-center items-center text-white"
+          >
+            <h1 className="text-xl font-bold mb-2">{project.name}</h1>
+            <p className="text-sm p-2">{project.textPreview}</p>
+          </div>
+        </a>
+      ) : (
+        <></>
+      )}
+    </>
   );
 }
